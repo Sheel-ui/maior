0fd9d97e3630c1cdbe73654d48c7db46a15c7c2e
Sheel Taskar <sheeltaskar64@gmail.com>
2024-11-09 13:29:01 -0500

    added experimental feature

--- maior-frontend/src/components/custom/Insights.tsx
+++ maior-frontend/src/components/custom/Insights.tsx
@@ -1,16 +1,67 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectItem } from "@/components/ui/select";
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "../ui/card";
+import { useState } from "react";
+
+const months = [
+	"City Spending Analysis",
+	"Spending Categories Breakdown",
+	"Restaurant Spending Insights",
+	"Frequent Merchants and Patterns",
+	"Unexpected High Expenditures",
+	"Top 5 Most Visited Merchants",
+	"Average Transaction Amount by Category",
+	"Biggest One-Time Purchases",
+	"Suggested Savings Areas",
+	"Comparing Spending to Historical Trends",
+	"Recurring Charges Analysis",
+	"Seasonal Spending Patterns",
+	"Travel Spending Analysis"
+  ];
+  
+
 
 export default function Insights() {
+	const [selectedPrompt, setSelectedPrompt] = useState("0");
 	return (
 		<Card className="h-[640px]">
 			<CardHeader>
-				<CardTitle className="text-md">Insights</CardTitle>
-				<CardDescription className="text-sm">
-					By ChatGpt
-				</CardDescription>
+				<div className="flex justify-between">
+					<div>
+						{" "}
+						<CardTitle className="text-md">Insights</CardTitle>
+						<CardDescription className="text-sm">
+							By ChatGpt
+						</CardDescription>
+					</div>
+					<Select value={selectedPrompt} onValueChange={setSelectedPrompt}>
+						<SelectTrigger className="w-[320px]">
+							<SelectValue />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectGroup>
+								{months.map((month, index) => (
+									<SelectItem
+										key={index}
+										value={index.toString()}
+									>
+										{month}
+									</SelectItem>
+								))}
+							</SelectGroup>
+						</SelectContent>
+					</Select>
+				</div>
 			</CardHeader>
-            <CardContent className="space-y-10 overflow-y-auto h-[525px]">
-            </CardContent>
+			<CardContent className="space-y-10 overflow-y-auto h-[525px] pt-4">
+				{selectedPrompt}
+			</CardContent>
 		</Card>
 	);
 }
--- maior-frontend/src/components/dashboard/Llm.tsx
+++ maior-frontend/src/components/dashboard/Llm.tsx
@@ -5,7 +5,7 @@ import { FlaskConical } from "lucide-react";
 export default function Llm() {
   return (
 		<div className="grid grid-cols-2 gap-8 px-8 pb-8 relative">
-			<div className="absolute right-10 -top-14 text-sm text-white bg-primary px-2 pr-4 py-1 rounded-md flex justify-center items-center">
+			<div className="absolute right-10 -top-14 text-sm bg-yellow-200 text-yellow-800 px-2 pr-4 py-1 rounded-md flex justify-center items-center">
 				<FlaskConical className="h-4"></FlaskConical>
 				<span>Experimental</span>
 			</div>
